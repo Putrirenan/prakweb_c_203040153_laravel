4b46ee2d67a941a6caf297a64c0808b8b70f8d1a
Putrirenan <putrirenan@gmail.com>
2022-10-17 18:54:53 +0700

    menyelesaikan video 3 Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Renandra Rahadian Putri",
+        "email" => "putrirenan@gmail.com",
+        "image" => "rere.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
