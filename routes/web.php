73f09a6c2657b028b544d1143c9f1086ca2065f6
Putrirenan <putrirenan@gmail.com>
2022-11-05 18:29:25 +0700

    menyelesaikan video 16 User Login & Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,7 +54,11 @@ Route::get('/categories', function () {
 });
 
 //  Halaman login
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 //  Halaman register
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+// Halaman Dashboard
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
